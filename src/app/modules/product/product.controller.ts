03309f2440588dcf628aeeae06fffce7ebce2910
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-07-10 09:26:32 +0600

    product added route complete

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,4 +1,23 @@
-const createProduct = () => {};
+import { catchAsync } from '../../utils/catchAsync';
+import { productServices } from './product.services';
+
+const createProduct = catchAsync(async (req, res, next) => {
+  const result = await productServices.createProductIntoDB(req.body);
+
+  res.status(200).json({
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+
+  //function generate response
+  //   sendResponse(res, {
+  //     statusCode: httpStatus.OK,
+  //     success: true,
+  //     message: 'Student is created successfully',
+  //     data: result,
+  //   });
+});
 
 export const productController = {
   createProduct,
--- src/app/modules/product/product.services.ts
+++ src/app/modules/product/product.services.ts
@@ -1,5 +1,14 @@
-const createProductIntoDB = () => {};
+import { TProduct } from './product.interfaces';
+import Product from './product.model';
+
+const createProductIntoDB = async (payload: TProduct) => {
+  const result = await Product.create(payload);
+  return result;
+};
+
+const getAllProductIntoDB = async (payload: TProduct) => {};
 
 export const productServices = {
   createProductIntoDB,
+  getAllProductIntoDB,
 };
