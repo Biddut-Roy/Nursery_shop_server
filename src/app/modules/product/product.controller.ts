6e7c750b8b576b74efbc1ed13e72144401f7e5f7
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-07-13 00:27:21 +0600

    send response function created

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,4 +1,6 @@
+import httpStatus from 'http-status';
 import { catchAsync } from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendrespons';
 import { productServices } from './product.services';
 
 const createProduct = catchAsync(async (req, res, next) => {
@@ -11,30 +13,24 @@ const createProduct = catchAsync(async (req, res, next) => {
   });
 
   //function generate response
-  //   sendResponse(res, {
-  //     statusCode: httpStatus.OK,
-  //     success: true,
-  //     message: 'Student is created successfully',
-  //     data: result,
-  //   });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
 });
 
 const getProduct = catchAsync(async (req, res, next) => {
   const result = await productServices.getAllProductIntoDB();
 
-  res.status(200).json({
+  // function generate response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: 'Product is retrieved successfully',
     data: result,
   });
-
-  //function generate response
-  //   sendResponse(res, {
-  //     statusCode: httpStatus.OK,
-  //     success: true,
-  //     message: 'Product is retrieved successfully',
-  //     data: result,
-  //   });
 });
 
 export const productController = {
